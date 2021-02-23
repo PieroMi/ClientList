b19182af1057a982099a4ae01af19c34fc6f6b3e
PieroMi <64490766+PieroMi@users.noreply.github.com>
2021-02-22 20:37:41 -0500

    Update Contact.java

--- ArrayListChallenge/src/Contact.java
+++ ArrayListChallenge/src/Contact.java
@@ -2,10 +2,14 @@
 public class Contact {
 	private String name;
 	private String idNumber;
+	private String favPizza;
 	
-	public Contact(String name, String idNumber) {
+	
+
+	public Contact(String name, String idNumber, String favPizza) {
 		this.name = name;
 		this.idNumber = idNumber;
+		this.favPizza = favPizza;
 	}
 
 	public String getName() {
@@ -16,7 +20,10 @@ public class Contact {
 		return idNumber;
 	}
 	
-	public static Contact createContact(String name, String idNumber) {
-		return new Contact(name, idNumber);
+	public String getFavPizza() {
+		return favPizza;
+	}
+	public static Contact createContact(String name, String idNumber, String favPizza) {
+		return new Contact(name, idNumber, favPizza);
 	}
 }
